999b21372c538ecea93e787939b43b9a6c191619
@Draichi <lucasdraichi@gmail.com>
2023-05-01 16:24:52 -0300

    wip: login after client.once

--- api/hello.js
+++ api/hello.js
@@ -6,13 +6,12 @@ dotenv.config();
 export default async function handler(request, response) {
   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-  client.login(process.env.DISCORD_BOT_TOKEN);
-
   const foo = client.once(Events.ClientReady, async (c) => {
     const guild = c.guilds.cache.get("1091486972616376441");
     const channel = guild.channels.cache.get("1102648245106257990");
     return await channel.send("foo bar baz");
   });
+  client.login(process.env.DISCORD_BOT_TOKEN);
 
   return response.end({
     foo,
