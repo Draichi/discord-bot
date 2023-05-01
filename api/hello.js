9ba64e69e7a147be09200f1d01e268d99249310a
@Draichi <lucasdraichi@gmail.com>
2023-05-01 16:25:59 -0300

    wip: removes return

--- api/hello.js
+++ api/hello.js
@@ -6,14 +6,14 @@ dotenv.config();
 export default async function handler(request, response) {
   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-  const foo = client.once(Events.ClientReady, async (c) => {
+  client.once(Events.ClientReady, async (c) => {
     const guild = c.guilds.cache.get("1091486972616376441");
     const channel = guild.channels.cache.get("1102648245106257990");
-    return await channel.send("foo bar baz");
+    await channel.send("foo bar baz");
   });
   client.login(process.env.DISCORD_BOT_TOKEN);
 
   return response.end({
-    foo,
+    success: true,
   });
 }
