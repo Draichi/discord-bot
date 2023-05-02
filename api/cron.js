0de39f521187908fca4d4b600df10de96d730fc5
@Draichi <lucasdraichi@gmail.com>
2023-05-02 02:42:26 -0300

    feat: add cron path

--- api/cron.js
+++ api/cron.js
@@ -4,43 +4,50 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const response = await fetch(
-  "https://www.awwwards.com/websites/sites_of_the_day/"
-);
-const r = await response.text();
+export default async function handler(_, response) {
+  const response = await fetch(
+    "https://www.awwwards.com/websites/sites_of_the_day/"
+  );
+  const r = await response.text();
 
-const dom = new JSDOM(r);
-const cardSite = dom.window.document.querySelector(".card-site");
-const websiteURL = cardSite.querySelector(".figure-rollover__bt").href;
-const websiteTitle = cardSite
-  .querySelectorAll(".figure-rollover__row")
-  .item(1).textContent;
-const websiteImage = cardSite
-  .querySelector(".figure-rollover__file")
-  .getAttribute("data-srcset")
-  .split(" ")[0];
+  const dom = new JSDOM(r);
+  const cardSite = dom.window.document.querySelector(".card-site");
+  const websiteURL = cardSite.querySelector(".figure-rollover__bt").href;
+  const websiteTitle = cardSite
+    .querySelectorAll(".figure-rollover__row")
+    .item(1).textContent;
+  const websiteImage = cardSite
+    .querySelector(".figure-rollover__file")
+    .getAttribute("data-srcset")
+    .split(" ")[0];
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.once(Events.ClientReady, async (c) => {
-  const guild = c.guilds.cache.get("1091486972616376441");
-  const channel = guild.channels.cache.get("1102648245106257990");
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-  const exampleEmbed = new EmbedBuilder()
-    .setColor(0x0099ff)
-    .setTitle("Site of the day")
-    .setThumbnail(websiteImage)
-    .addFields(
-      { name: "Website URL", value: websiteURL },
-      { name: "Webstie Title", value: websiteTitle },
-      { name: "Website title", value: "Some value here", inline: true },
-      { name: "Inline field title", value: "Some value here", inline: true }
-    )
-    .setImage(websiteImage)
-    .setTimestamp();
+  client.once(Events.ClientReady, async (c) => {
+    const guild = c.guilds.cache.get("1091486972616376441");
+    const channel = guild.channels.cache.get("1102648245106257990");
 
-  await channel.send({
-    embeds: [exampleEmbed],
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099ff)
+      .setTitle("Site of the day")
+      .setThumbnail(websiteImage)
+      .addFields(
+        { name: "Website URL", value: websiteURL },
+        { name: "Webstie Title", value: websiteTitle },
+        { name: "Website title", value: "Some value here", inline: true },
+        { name: "Inline field title", value: "Some value here", inline: true }
+      )
+      .setImage(websiteImage)
+      .setTimestamp();
+
+    const discordResponse = await channel.send({
+      embeds: [exampleEmbed],
+    });
+
+    return response.send({
+      ...discordResponse,
+    });
   });
-});
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+  client.login(process.env.DISCORD_BOT_TOKEN);
+}
