33e82d30e9483381267d515b2897f59af44a97bf
@Draichi <lucasdraichi@gmail.com>
2023-05-01 21:32:12 -0300

    wip: cron

---
+++ api/cron.js
@@ -0,0 +1,20 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export default async function handler(_, response) {
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+  client.once(Events.ClientReady, async (c) => {
+    const guild = c.guilds.cache.get("1091486972616376441");
+    const channel = guild.channels.cache.get("1102648245106257990");
+    const discordResponse = await channel.send("foo bar baz");
+
+    return response.send({
+      ...discordResponse,
+    });
+  });
+
+  client.login(process.env.DISCORD_BOT_TOKEN);
+}
