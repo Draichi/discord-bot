eb5e0b26e32a88791e983ee457f6cf781f54caf8
@Draichi <lucasdraichi@gmail.com>
2023-05-07 21:55:04 -0300

    feat: add typescript trending repos

--- api/warm-cron.js
+++ api/warm-cron.js
@@ -1,4 +1,5 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
+import { JSDOM } from "jsdom";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -6,16 +7,65 @@ dotenv.config();
 export default async function handler(_, response) {
   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-  await client.login(process.env.DISCORD_BOT_TOKEN);
+  const githubTypescriptTrendingPage = await fetch(
+    "https://github.com/trending/typescript?since=daily"
+  );
+
+  const r = await githubTypescriptTrendingPage.text();
+
+  const dom = new JSDOM(r);
+  const repositoryCards =
+    dom.window.document.querySelectorAll("article.Box-row");
+
+  const res = [];
+  for (let i = 0; i < 5; i++) {
+    const repositoryCard = repositoryCards.item(i);
+    const repositoryCreator = repositoryCard.querySelector("h2.h3 > a");
+    const description = repositoryCard.querySelector("p").textContent.trim();
+    const repositoryLink = `https://github.com${repositoryCreator.getAttribute(
+      "href"
+    )}`;
+    const repositoryTitle = repositoryCreator
+      .querySelector("span")
+      .textContent.split("/")[0]
+      .trim();
+    const repositoryName = repositoryCreator.textContent.split("/")[1].trim();
+
+    res.push({
+      repositoryLink,
+      repositoryTitle,
+      description,
+      repositoryName,
+    });
+  }
+
+  client.login(process.env.DISCORD_BOT_TOKEN);
 
   client.once("ready", async (c) => {
     const guild = c.guilds.cache.get("1091486972616376441");
     const channel = guild.channels.cache.get("1102648245106257990");
 
-    const discordResponse = await channel.send("Quais serão os sites de hoje?");
+    const embeds = [];
 
-    return response.send({
-      ...discordResponse,
+    res.forEach((item) => {
+      const { description, repositoryTitle, repositoryLink, repositoryName } =
+        item;
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+      const embededPost = new EmbedBuilder()
+        .setColor(+`0x${randomColor}`)
+        .setTitle(repositoryTitle)
+        .setDescription(description)
+        .setURL(repositoryLink);
+
+      embeds.push(embededPost);
     });
+
+    const discordResponse = await channel.send({
+      content: "Top 5 trending TypeScript repositories on GitHub today:",
+      embeds,
+    });
+
+    return response.send(discordResponse);
   });
 }
